63a3e946ad64dc8be89268422232a668ffa42f10
NateScarlet <NateScarlet@Gmail.com>
2022-08-22 01:11:24 +0800

    chore: disable i18n warning

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,8 +1,11 @@
 import messages from '@intlify/vite-plugin-vue-i18n/messages';
-import type { UseI18nOptions } from 'vue-i18n';
+import { defineComponent, h, useAttrs, useSlots } from 'vue';
 import { createI18n, useI18n as _useI18n } from 'vue-i18n';
+import { isDevelopmentMode } from '@/settings';
 
 export const supportedLanguages = Object.keys(messages);
+
+const defaultFallback = 'en';
 
 export const locale = ((): string => {
   if (supportedLanguages.includes(navigator.language)) {
@@ -16,31 +19,50 @@ export const locale = ((): string => {
       return ret;
     }
   }
-  return import.meta.env.VUE_APP_I18N_LOCALE ?? 'en';
+  return import.meta.env.VUE_APP_I18N_LOCALE || defaultFallback;
 })();
 
-export function useI18n(options?: UseI18nOptions): ReturnType<
-  typeof _useI18n
-> & {
-  td: (key: string, defaultValue?: string) => string;
-} {
-  const i18n = _useI18n(options);
-  const td = (key: string, defaultValue?: string): string => {
-    if (defaultValue != null) {
-      return i18n.te(key) ? i18n.t(key).toString() : defaultValue;
-    }
-    return i18n.t(key).toString();
-  };
+const i18n = createI18n({
+  legacy: true,
+  locale: import.meta.env.VUE_APP_I18N_LOCALE || locale || defaultFallback,
+  fallbackLocale:
+    import.meta.env.VUE_APP_I18N_FALLBACK_LOCALE || defaultFallback,
+  messages,
+});
 
-  return {
-    ...i18n,
-    td,
+if (isDevelopmentMode) {
+  const rawInstall = i18n.install;
+  i18n.install = (app, options) => {
+    rawInstall(app, options);
+    // eslint-disable-next-line no-console
+    console.log(
+      'patching i18nT to disable scope warning\nhttps://github.com/intlify/vue-i18n-next/discussions/962'
+    );
+    const rawI18nT = app.component('i18n-t');
+    if (!rawI18nT) {
+      throw new Error('missing i18n-t component');
+    }
+    const i18nT = defineComponent({
+      render() {
+        return h(
+          rawI18nT as any,
+          { scope: 'global', ...useAttrs() },
+          useSlots()
+        );
+      },
+    });
+
+    // eslint-disable-next-line vue/component-definition-name-casing
+    app.component('i18n-t', i18nT);
   };
 }
 
-export default createI18n({
-  legacy: false,
-  locale,
-  fallbackLocale: import.meta.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages,
-});
+export function tr(key: string, d?: string): string {
+  const { te, t } = i18n.global;
+  if (d != null) {
+    return te(key) ? t(key).toString() : d;
+  }
+  return t(key).toString();
+}
+
+export default i18n;
